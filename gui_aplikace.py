ae13de2c283463d4446cdb870fa4807b94cd0231
doutnma20 <doutnma20@sps-prosek.cz>
2022-03-01 14:11:59 +0100

    Commit2

--- gui_aplikace.py
+++ gui_aplikace.py
@@ -1,9 +1,22 @@
 import tkinter as tk
+tk.Tk()
 
 window = tk.Tk()
-window.geometry("250x250")
-window.title("Aplikace v1.0 beta")
-label1 = tk.Label(text="Ahoj!!!")
+window.geometry("100x100")
+window.title("Testík")
+
+"""
+Label
+Entry
+Button
+Text
+"""
+
+label1 = tk.Label(
+    text="Moje aplikace",
+    background="black",
+    foreground="white"
+)
 label1.pack()
 
 window.mainloop()
