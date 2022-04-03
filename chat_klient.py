6c72789fd12901245018a65bf28b895f7ff42be7
Martin Doutnáč <doutnma20@sps-prosek.cz>
2022-04-03 22:09:34 +0200

    finální změny

--- chat_klient.py
+++ chat_klient.py
@@ -1,134 +1,128 @@
 import socket, threading
 import tkinter as tk
+from tkinter import *
 import time
+from tkinter import END
+global nick
 
 def prichozi_zpravy(connection: socket.socket):
+    text.grid(sticky="W")
+    text.configure(state='disabled')
     while True:
         try:
             msg = connection.recv(1024)
             if msg:
                 print(msg.decode())
+                text.configure(state='normal')
+                aaa = str(msg.decode())
+                text.insert(END, aaa + '\n')
+                text.configure(state='disabled')
             else:
                 connection.close()
                 break
         except Exception as e:
-            print(f'Error handling message from server: {e}')
+            print(f'Error: {e}')
             connection.close()
             break
-
 def login():
     hostval = ip.get()
     portval = port.get()
-    try:
-        s.connect((hostval, int(portval)))
-        threading.Thread(target=prichozi_zpravy, args=[s]).start()
-        print('Připojeno do chatovací místnosti')
+    prezdivka = name.get()
 
-        ip.grid_remove()
-        labelIP.grid_remove()
-        port.grid_remove()
-        labelPort.grid_remove()
-        enterLogin.grid_remove()
-
-        labelName.grid_remove()
-        name.grid_remove()
-
-        errMsg = tk.Label(text="Připojeno!")
-
+    if prezdivka != "":
+        try:
+            s.connect((hostval, int(portval)))
+            threading.Thread(target=prichozi_zpravy, args=[s]).start()
+            print('Připojeno do chatíku')
+            ip.grid_remove()
+            labelIP.grid_remove()
+            port.grid_remove()
+            labelPort.grid_remove()
+            enterLogin.grid_remove()
+            labelName.grid_remove()
+            name.grid_forget()
+            errMsg = tk.Label(text="Připojeno!!!")
+            errMsg.grid(sticky="W")
+            time.sleep(1)
+            errMsg.grid_remove()
+            text.configure(state='disabled')
+            messageText.grid(sticky="N")
+            sendButton.grid(sticky="S")
+        except Exception as e:
+            print(f'Error{e}')
+            ip.delete(0, tk.END)
+            port.delete(0, tk.END)
+            errMsg=tk.Label(text="Error")
+            errMsg.grid(sticky="W")
+            time.sleep(1)
+            errMsg.grid_remove()
+    else:
+        errMsg2 = tk.Label(text="Vaše přezdívka:")
+        errMsg2.grid(sticky="W")
         time.sleep(1)
-        errMsg.grid_remove()
+        errMsg2.grid_remove()
 
-        messageText.grid(sticky="N")
-        sendButton.grid(sticky="S")
-
-    except Exception as e:
-        print(f'Error při připojování k serveru {e}')
-        ip.delete(0, tk.END)
-        port.delete(0, tk.END)
-        errMsg = tk.Label(text="Error při připojování k serveru")
-        errMsg.grid(sticky="N")
-
-def send():
+def sendMsg():
+    text.configure(state='normal')
     msg = messageText.get()
     s.send(msg.encode())
+    if msg != "":
+        msg1 = "Moje zpráva - {}".format(msg)
+        text.insert(END, msg1 + '\n')
+        messageText.delete(0, tk.END)
+    text.configure(state='disabled')
 
 window = tk.Tk()
 window.geometry("400x300")
 window.title("Četík")
 s = socket.socket()
 
-labelIP = tk.Label(
-    text="Zadejte IP adresu serveru",
+labelIP=tk.Label(
+    text = "Zadejte IP adresu serveru",
+    width = 20,
+    fg="red",
+    bg="black"
+)
+ip=tk.Entry()
+
+labelPort=tk.Label(
+    text = "Zadejte Port",
+    width=20,
+    fg="red",
+    bg="black"
+)
+port=tk.Entry()
+
+labelIP.grid(row=1, column=0)
+ip.grid(row=1, column=2)
+
+labelPort.grid(row=2, column=0)
+port.grid(row=2, column=2)
+
+labelName=tk.Label(
+    text = "Přezdívka",
     fg="red",
     bg="black",
     width=20
 )
-ip = tk.Entry(
-    width=20
-)
-labelPort = tk.Label(
-    text="Zadejte Port",
-    fg="red",
-    bg="black",
-    width=20
-)
-port = tk.Entry(
-    width=20
+name=tk.Entry()
+
+labelName.grid(row=3, column=0)
+name.grid(row=3, column=2)
+
+enterLogin=tk.Button(text = "Připojit se do četíku", command=login)
+enterLogin.grid(row=4, column=2)
+
+messageText = tk.Entry(
+    width=10,
 )
 
-labelIP.grid(
-    sticky="S",
-    column=0,
-    row = 1
-)
-ip.grid(
-    sticky="S",
-    column=1,
-    row=1
-)
-labelPort.grid(
-    sticky="S",
-    column=0,
-    row=2
-)
-port.grid(
-    sticky="S",
-    column=1,
-    row=2
-)
-enterLogin = tk.Button(
-    text="Připojit",
-    command=login,
-    width= 20
-)
-enterLogin.grid(
-    sticky="S",
-    column=1,
-    row=3
+sendButton = tk.Button(
+    text="Poslat",
+    command=sendMsg,
+    width=10
 )
 
-labelName = tk.Label(text="Přezdívka")
-name = tk.Entry()
-
-messageText = tk.Entry()
-sendButton = tk.Button(text="Odeslat", command=send)
+text = tk.Text(width=60, height=10)
 
 window.mainloop()
-
-host = input(str('Host / IP : '))
-port = input(str('Port : '))
-
-try:
-    s.connect((host, int(port)))
-    threading.Thread(target=prichozi_zpravy, args=[s]).start()
-    print('Připojeno do chatovací místnosti')
-    while True:
-        msg = input('>>')
-        if msg == 'q_app_q_69':
-            break
-        s.send(msg.encode())
-        s.close()
-
-except Exception as e:
-    print(f'Error při připojování k serveru {e}')
-    s.close()
--- server.py
+++ server.py
@@ -1,15 +1,26 @@
 import socket, threading
+import logging
+from datetime import datetime
+import os
+import sys
+
+logname = datetime.now()
+
+logger = logging.getLogger()
+
+logger.setLevel(logging.DEBUG)
 
 aktivni_users = []
 
 def rozesilani_zprav(connection: socket.socket, address: str):
+
     while True:
         try:
             msg = connection.recv(1024)
-
             if msg:
-                print(f'{address[0]}:{address[1]} - {msg.decode()}')
-                msg_to_send = f'Od {address[0]}:{address[1]} - {msg.decode()}'
+                print(f' uživatel {address[0]}:{address[1]} napsal {msg.decode()}')
+                logger.info(f'{address[0]}:{address[1]} - {msg.decode()}')
+                msg_to_send = f'{address[0]}:{address[1]} - {msg.decode()}'
                 for client_conn in aktivni_users:
                     if client_conn != connection:
                         try:
@@ -19,32 +30,36 @@ def rozesilani_zprav(connection: socket.socket, address: str):
                             if connection in aktivni_users:
                                 connection.close()
                                 aktivni_users.remove(connection)
-
             else:
                 if connection in aktivni_users:
                     connection.close()
                     aktivni_users.remove(connection)
                 break
-
         except Exception as e:
             print(f'Error při navazování spojení: {e}')
             if connection in aktivni_users:
                 connection.close()
                 aktivni_users.remove(connection)
             break
-
 s = socket.socket()
 host = socket.gethostname()
-print(' Serverík běží na adrese : ', host)
-port = 3302
-print(' Portíček : ', port)
+print(' Adresa Serveríku: ', host)
+
+port = 8080
+print(' Portík: ', port)
 s.bind((host, port))
 
+logger.debug("Server je spuštěn")
+logger.debug(f'Adresa serveru : {host}')
+logger.debug(f'Port serveru : {port}')
+
 while True:
-    s.listen()
+    s.listen(10)
     socket_connection, address = s.accept()
-    print(address, 'Připojeno')
-    hi = " {} se připojil!".format(address)
+    print(address, ' připojen')
+    logger.info(f'{address[0]}:{address[1]} - připojen')
+    hi = " {} připojen!".format(address)
+
     socket_connection.sendall(str.encode(hi))
 
     for client_conn in aktivni_users:
@@ -52,9 +67,11 @@ while True:
             client_conn.send(hi.encode())
         except Exception as e:
             print(f'Error při navazování spojení: {e}')
+            logger.warning(f'Error při navazování spojení-{address[0]}:{address[1]}')
             if socket_connection in aktivni_users:
                 socket_connection.close()
                 aktivni_users.remove(socket_connection)
+                logger.warning(f'Neaktivní uživatel byl odstraněn -{address[0]}:{address[1]}')
             break
     aktivni_users.append(socket_connection)
     threading.Thread(target=rozesilani_zprav, args=[socket_connection, address]).start()
